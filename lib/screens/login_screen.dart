3b1e93ebf9ba95b31fe2c385df911fd3d20f65cf
Blessing Wisdom <prodigeebw@gmail>
2022-05-09 16:12:28 +0100

    Working on login Screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
